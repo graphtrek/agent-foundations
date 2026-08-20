7983952e79d67a6c6b1d78ccf1b14f93876ac152
Imre <imre.tatai@gmail.com>
2026-08-20 03:04:43 +0200

    Implement configuration loading and agent functionality

---
+++ src/config.py
@@ -0,0 +1,26 @@
+import os
+import getpass
+from dotenv import load_dotenv, find_dotenv
+
+def load_config() -> str:
+    env_path = find_dotenv()
+    if env_path:
+        print(f"Loading environment variables from: {env_path}")
+        load_dotenv(env_path)
+    else:
+        print("No .env file found. Please ensure that the .env file exists in the project directory.")      
+
+    api_key = os.environ.get("OPENROUTER_API_KEY")
+    if api_key is None:
+        print("Please enter your OpenRouter API key:")
+        api_key = getpass.getpass("API Key: ")
+        os.environ["OPENROUTER_API_KEY"] = api_key
+
+    if api_key and api_key.strip():
+        print('OPENROUTER_API_KEY environment variable is set.')
+    else:
+        print("OPENROUTER_API_KEY environment variable is not set.")
+
+    if os.environ.get("LANGCHAIN_DEBUG") == "true":
+        print("LANGCHAIN_DEBUG is set to true. Debugging information will be printed.")
+    return api_key
--- src/simple_agent.py
+++ src/simple_agent.py
@@ -1,34 +1,8 @@
-import os
-import getpass
-from urllib import response
-from dotenv import load_dotenv, find_dotenv
+from config import load_config
 from langchain.chat_models import init_chat_model
 from langchain.agents import create_agent
 from pprint import PrettyPrinter
 from langchain.messages import HumanMessage, AIMessage, SystemMessage
-
-def load_config() -> str:
-    env_path = find_dotenv()
-    if env_path:
-        print(f"Loading environment variables from: {env_path}")
-        load_dotenv(env_path)
-    else:
-        print("No .env file found. Please ensure that the .env file exists in the project directory.")      
-
-    api_key = os.environ.get("OPENROUTER_API_KEY")
-    if api_key is None:
-        print("Please enter your OpenRouter API key:")
-        api_key = getpass.getpass("API Key: ")
-        os.environ["OPENROUTER_API_KEY"] = api_key
-
-    if api_key and api_key.strip():
-        print(f'OPENROUTER_API_KEY environment variable is set to: {api_key}')
-    else:
-        print("OPENROUTER_API_KEY environment variable is not set.")
-
-    if os.environ.get("LANGCHAIN_DEBUG") == "true":
-        print("LANGCHAIN_DEBUG is set to true. Debugging information will be printed.")
-    return api_key
 
 def simple_agent(model_name: str):
     api_key = load_config()
